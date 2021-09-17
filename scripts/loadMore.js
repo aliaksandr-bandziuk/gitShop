edea65ba60b02a72a84b2974447291ec8fb97c16
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2021-09-18 01:12:27 +0300

    feat: create catalog page

--- scripts/loadMore.js
+++ scripts/loadMore.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  $(".content").slice(0, 4).show();
+  $(".content").slice(0, 8).show();
   $("#loadMore").on("click", function(e){
     e.preventDefault();
     $(".content:hidden").slice(0, 4).slideDown();
     if($(".content:hidden").length == 0) {
-      $("#loadMore").text("Вы посмотрели все товары категории").addClass("noContent");
+      $("#loadMore").text("Вы посмотрели все товары").addClass("noContent");
     }
   });
   
