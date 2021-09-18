8ab0065375494a5d392a61d02c62ce8d12ea0d47
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2021-09-18 15:30:39 +0300

    fix: edit load more catalog

--- scripts/loadMore.js
+++ scripts/loadMore.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  $(".content").slice(0, 8).show();
+  $(".content").slice(0, 6).show();
   $("#loadMore").on("click", function(e){
     e.preventDefault();
-    $(".content:hidden").slice(0, 4).slideDown();
+    $(".content:hidden").slice(0, 3).slideDown();
     if($(".content:hidden").length == 0) {
       $("#loadMore").text("Вы посмотрели все товары").addClass("noContent");
     }
